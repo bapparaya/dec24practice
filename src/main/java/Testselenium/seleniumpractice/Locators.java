112d50bfca267f9cd03014df3712e4198ebbd25c
BapparayaR <BapparayaR@KAN-DEVLAP137.grasko.com>
2024-12-10 08:57:21 +0530

    okay

---
+++ src/main/java/Testselenium/seleniumpractice/Locators.java
@@ -0,0 +1,42 @@
+package Testselenium.seleniumpractice;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators {
+
+
+	public static void main(String[] args) {
+		
+	 
+		
+	 WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.zoho.com/en-in/crm/");
+		driver.manage().window().maximize();
+		driver.findElement(By.className("zgh-login")).click();
+		try{
+			Thread.sleep(1000);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		Boolean isdisplay = driver.findElement(By.id("banner_0")).isDisplayed();
+		System.out.println(isdisplay);
+		
+		//driver.findElement(By.partialLinkText("Learn m")).click();
+		
+		List<WebElement> elms = driver.findElements(By.id("fed_signin_options"));
+		elms.size();
+		
+
+	}
+
+}
